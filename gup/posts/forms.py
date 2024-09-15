fb3ed78a25811fdb6f2b334e18442680cea350de
SteakTheStake <gabecorr25@gmail.com>
2024-09-15 02:33:11 +0200

    0.0.2f

--- gup/posts/forms.py
+++ gup/posts/forms.py
@@ -1,16 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, FileField
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
 
-
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = FileField(u'Image', validators=[FileRequired()])
-    description = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-content = FileField('Upload', [
-        FileAllowed(['jpg', 'jpeg', 'png'],
-                    'Only "jpg", "jpeg" and "png" files are supported')
-    ])
+  title = StringField('Title', validators=[DataRequired()])
+  content = FileField('Upload Image', validators=[
+      FileAllowed(['jpg', 'jpeg', 'png'], 'Only jpg, jpeg and png files are allowed')
+  ])
+  description = TextAreaField('Description', validators=[DataRequired()])
+  submit = SubmitField('Post')
--- gup/posts/routes.py
+++ gup/posts/routes.py
@@ -1,3 +1,6 @@
+from flask import url_for, request
+from werkzeug.utils import secure_filename
+import os
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
@@ -11,19 +14,27 @@ posts = Blueprint('posts', __name__)
 @posts.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
-    form = PostForm()
-    image_file = url_for('static', filename='user_img/<string:username>' + image_file)
-    if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post has been created!', 'success')
-        return redirect(url_for('main.home'))
-    return render_template('create_post.html', title='New Post',
-                           image_file=image_file, form=form, legend='New Post')
+  form = PostForm()
+  if form.validate_on_submit():
+      if form.content.data:
+          filename = secure_filename(form.content.data.filename)
+          user_dir = os.path.join(current_app.root_path, 'static', 'user_img', current_user.username)
+          os.makedirs(user_dir, exist_ok=True)
+          file_path = os.path.join(user_dir, filename)
+          form.content.data.save(file_path)
+          image_file = url_for('static', filename=f'user_img/{current_user.username}/{filename}')
+      else:
+          image_file = None
+
+      post = Post(title=form.title.data, content=image_file, description=form.description.data, author=current_user)
+      db.session.add(post)
+      db.session.commit()
+      flash('Your post has been created!', 'success')
+      return redirect(url_for('main.home'))
+  return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
-@posts.route("/post/<int:post_id>")
+posts.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
